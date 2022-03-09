df3f936294c25251bd5bf6eb57dd16bc63346700
NicolasBarretoDesarrolloWeb <nicolasb21@outlook.com>
2022-03-09 20:04:56 -0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Container, Wrapper } from "./styles";
+import { Container, Wrapper, Wrapper2} from "./styles";
 export const Header = () => {
   return (
 
 <Container>
+
   <Wrapper>
     <img src="https://http2.mlstatic.com/frontend-assets/ui-navigation/5.18.9/mercadolibre/logo__large_plus.png" alt="logo"/>
   </Wrapper>
+  <Wrapper2>
+  
+  <input type="search" action="https://www.mercadolibre.com.ar/jm/search" className='lupa' placeholder='Buscar productos, marcas y más...'/>
+  
+  </Wrapper2> 
 </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,11 +8,44 @@ height:100px;
 export const Wrapper = styled.nav`
 max-width:1200px;
 padding: 8px 8px;
-margin: 0 auto;
-
+margin-left:350px;
+float:left;
 height: 91px;
 img{
    width:134px;
    heigth:33px; 
 }
+
+
+`
+export const Wrapper2 = styled.div`
+
+    transition: left .15s ease-out;
+    position: absolute;
+    left: 530px;
+    top: 10;
+    right: 45px;
+    height: 50px;
+    padding: 8px 0;
+    width:300px;
+   
+}
+.lupa{
+   width:598px;
+   height: 39px;
+   size:300;
+}
+
+
+}
+button.nav-search-btn, button.nav-search-btn:focus {
+top: 0;
+height: 50px;
+padding: 0;
+width: 48px;
+background: 0 0;
+border: none;
+font-size: 22px;
+color: #666;
+line-height: 1em;}
 `
